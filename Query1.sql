7d7dc8b68d24d211152ba8942f218a5ef5449a5c
Incognito <89413148+incognito0007@users.noreply.github.com>
2023-06-01 23:55:42 +0530

    Add files via upload
    SQL command for adding database.

---
+++ Query1.sql
@@ -0,0 +1,10 @@
+create database bankmanagementsystem;
+
+show databases;
+
+use bankmanagementsystem;
+create table signup(formno varchar(20), name varchar(30), father_name varchar(20), dob varchar(20), gender varchar(20), email varchar(30), marital_status varchar(20), address varchar(50), city varchar(25), state varchar(20), pincode varchar(20));
+
+show tables;
+
+select * from signup;
